bd07122fac3934d2d0f1cb2b62095cbb7b0dfe66
ZhielVn <59718363+ZhilVaniAzzuar@users.noreply.github.com>
2024-03-06 16:59:36 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          back: "#F9D9AA",
+          "back-dark": "#FFA966",
+          coffee: "#2F2105",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
